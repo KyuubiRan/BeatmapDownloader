337a6f4f3cabf3b23f0040842a0029130cf1a192
Kitsune <KyuubiRan@qq.com>
2023-06-19 22:12:32 +0800

    Add Most play downloader & fix 100 limit

--- Downloader/src/features/MultiDownload.cpp
+++ Downloader/src/features/MultiDownload.cpp
@@ -9,6 +9,12 @@ using namespace features;
 
 MultiDownload::MultiDownload() :
     Feature() {
+    t_SearchThread = std::thread(SearchThread);
+    t_SearchThread.detach();
+}
+
+void MultiDownload::doRequest(const Task &task) {
+    m_Queue.push_back(task);
 }
 
 ui::main::FeatureInfo &MultiDownload::getInfo() {
@@ -63,46 +69,54 @@ void DoBPDownload(int id, int beg, int end) {
         return;
     }
 
-    int offset = beg;
-    int limit = end - beg;
+    GuiHelper::ShowInfoToast(lang.getTextCStr("StartDownloadBP"), id, beg + 1, end);
+    MultiDownload::Task req;
+    req.url = std::format("https://osu.ppy.sh/users/{}/scores/best?mode={}&", id,
+                          bpDownloadSelectItems[bpDownloadSelect]);
+    req.id = id;
+    req.beg = beg;
+    req.end = end;
+    req.callback = [&](const int code, std::string &response, MultiDownload::Task &thiz) {
+        switch (code) {
+        case 200: {
+            try {
+                if (nlohmann::json j = nlohmann::json::parse(response); j.is_array() && !j.empty()) {
+                    if (j.size() < 100)
+                        thiz.canceled = true;
+                    downloader::BeatmapInfo bi{downloader::BeatmapType::Bid, -1, true};
+                    for (auto &i : j) {
+                        if (!i.contains("beatmap_id"))
+                            continue;
 
-    const auto link = std::format("https://osu.ppy.sh/users/{}/scores/best?mode={}&limit={}&offset={}", id,
-                                  bpDownloadSelectItems[bpDownloadSelect], limit, offset);
-    std::string response;
-    int code;
-    if (net::curl_get(link.c_str(), response, &code) != CURLE_OK)
-        return;
-
-    switch (code) {
-    case 200: {
-        try {
-            if (nlohmann::json j = nlohmann::json::parse(response); j.is_array() && !j.empty()) {
-                GuiHelper::ShowInfoToast(lang.getTextCStr("StartDownloadBP"), id, beg + 1, end);
-
-                downloader::BeatmapInfo bi{downloader::BeatmapType::Bid, -1, true};
-                for (auto &i : j) {
-                    bi.id = i["beatmap_id"];
-                    LOGD("Post bp download: bid=%d", bi.id);
-                    Downloader::GetInstance().postSearch(bi);
+                        bi.id = i["beatmap_id"];
+                        LOGD("Post bp download: bid=%d", bi.id);
+                        Downloader::GetInstance().postSearch(bi);
+                    }
                 }
+            } catch (...) {
+                LOGW("Cannot parse bp download response!");
+                LOGD("Response: %s", response.c_str());
+                thiz.canceled = true;
+                GuiHelper::ShowWarnToast(lang.getTextCStr("BPDownloadFailedParseFailed"));
             }
-        } catch (...) {
-            LOGW("Cannot parse bp download response!");
-            GuiHelper::ShowWarnToast(lang.getTextCStr("BPDownloadFailedParseFailed"));
+            break;
         }
-        break;
-    }
-    case 404: {
-        LOGW("BP Download failed: user(%d) not found!", id);
-        GuiHelper::ShowWarnToast(lang.getTextCStr("BPDownloadFailedUserNotFound"), id);
-        break;
-    }
-    default: {
-        LOGW("BP Download failed with http code: %d", code);
-        GuiHelper::ShowWarnToast(lang.getTextCStr("BPDownloadFailedResponseCodeNotOk"), code);
-        break;
-    }
-    }
+        case 404: {
+            LOGW("BP Download failed: user(%d) not found!", thiz.id);
+            thiz.canceled = true;
+            GuiHelper::ShowWarnToast(lang.getTextCStr("BPDownloadFailedUserNotFound"), thiz.id);
+            break;
+        }
+        default: {
+            LOGW("BP Download failed with http code: %d", code);
+            thiz.canceled = true;
+            GuiHelper::ShowWarnToast(lang.getTextCStr("BPDownloadFailedResponseCodeNotOk"), code);
+            break;
+        }
+        }
+    };
+
+    MultiDownload::GetInstance().doRequest(req);
 }
 
 void DoFavDownload(int id, int beg, int end) {
@@ -115,42 +129,54 @@ void DoFavDownload(int id, int beg, int end) {
         return;
     }
 
-    const auto link = std::format("https://osu.ppy.sh/users/{}/beatmapsets/favourite?limit={}&offset={}", id, end - beg, beg);
-    std::string response;
-    int code;
-    if (net::curl_get(link.c_str(), response, &code) != CURLE_OK)
-        return;
+    GuiHelper::ShowInfoToast(lang.getTextCStr("StartDownloadFav"), id, beg + 1, end);
 
-    switch (code) {
-    case 200: {
-        try {
-            if (nlohmann::json j = nlohmann::json::parse(response); j.is_array() && !j.empty()) {
-                GuiHelper::ShowInfoToast(lang.getTextCStr("StartDownloadFav"), id, beg + 1, end);
+    MultiDownload::Task req;
+    req.url = std::format("https://osu.ppy.sh/users/{}/beatmapsets/favourite?", id);
+    req.beg = beg;
+    req.end = end;
+    req.id = id;
+    req.callback = [&](const int code, std::string &response, MultiDownload::Task &thiz) {
+        switch (code) {
+        case 200: {
+            try {
+                if (nlohmann::json j = nlohmann::json::parse(response); j.is_array() && !j.empty()) {
+                    if (j.size() < 100)
+                        thiz.canceled = true;
+                    downloader::BeatmapInfo bi{downloader::BeatmapType::Sid, -1, true};
+                    for (auto &i : j) {
+                        if (!i.contains("id"))
+                            continue;
 
-                downloader::BeatmapInfo bi{downloader::BeatmapType::Sid, -1, true};
-                for (auto &i : j) {
-                    bi.id = i["id"];
-                    LOGD("Post bp download: bid=%d", bi.id);
-                    Downloader::GetInstance().postSearch(bi);
+                        bi.id = i["id"];
+                        LOGD("Post bp download: bid=%d", bi.id);
+                        Downloader::GetInstance().postSearch(bi);
+                    }
                 }
+            } catch (...) {
+                LOGW("Cannot parse Fav download response!");
+                LOGD("Response: %s", response.c_str());
+                thiz.canceled = true;
+                GuiHelper::ShowWarnToast(lang.getTextCStr("FavDownloadFailedParseFailed"));
             }
-        } catch (...) {
-            LOGW("Cannot parse Fav download response!");
-            GuiHelper::ShowWarnToast(lang.getTextCStr("FavDownloadFailedParseFailed"));
+            break;
         }
-        break;
-    }
-    case 404: {
-        LOGW("Fav Download failed: user(%d) not found!", id);
-        GuiHelper::ShowWarnToast(lang.getTextCStr("FavDownloadFailedUserNotFound"), id);
-        break;
-    }
-    default: {
-        LOGW("Fav Download failed with http code: %d", code);
-        GuiHelper::ShowWarnToast(lang.getTextCStr("FavDownloadFailedResponseCodeNotOk"), code);
-        break;
-    }
-    }
+        case 404: {
+            LOGW("Fav Download failed: user(%d) not found!", thiz.id);
+            thiz.canceled = true;
+            GuiHelper::ShowWarnToast(lang.getTextCStr("FavDownloadFailedUserNotFound"), thiz.id);
+            break;
+        }
+        default: {
+            LOGW("Fav Download failed with http code: %d", code);
+            thiz.canceled = true;
+            GuiHelper::ShowWarnToast(lang.getTextCStr("FavDownloadFailedResponseCodeNotOk"), code);
+            break;
+        }
+        }
+    };
+
+    MultiDownload::GetInstance().doRequest(req);
 }
 
 enum class BeatmapStatus : int {
@@ -180,44 +206,117 @@ void DoMapperDownload(int id, int beg, int end, const BeatmapStatus &stat) {
         return;
     }
 
-    const auto link = std::format("https://osu.ppy.sh/users/{}/beatmapsets/{}?limit={}&offset={}", id,
-                                  s_StatusStr[(int)stat], end - beg + 1, beg);
+    GuiHelper::ShowInfoToast(lang.getTextCStr("StartDownloadMapper"), id, beg + 1, end, s_StatusStr[(int)stat]);
 
-    std::string response;
-    int code;
-    if (net::curl_get(link.c_str(), response, &code) != CURLE_OK)
-        return;
+    MultiDownload::Task req;
+    req.url = std::format("https://osu.ppy.sh/users/{}/beatmapsets/{}?", id,
+                          s_StatusStr[(int)stat]);
+    req.beg = beg;
+    req.end = end;
+    req.id = id;
+    req.callback = [&](const int code, std::string &response, MultiDownload::Task &thiz) {
+        switch (code) {
+        case 200: {
+            try {
+                if (nlohmann::json j = nlohmann::json::parse(response); j.is_array() && !j.empty()) {
+                    if (j.size() < 100)
+                        thiz.canceled = true;
 
-    switch (code) {
-    case 200: {
-        try {
-            if (nlohmann::json j = nlohmann::json::parse(response); j.is_array() && !j.empty()) {
-                GuiHelper::ShowInfoToast(lang.getTextCStr("StartDownloadMapper"), id, beg + 1, end, s_StatusStr[(int)stat]);
+                    downloader::BeatmapInfo bi{downloader::BeatmapType::Sid, -1, true};
+                    for (auto &i : j) {
+                        if (!i.contains("id"))
+                            continue;
 
-                downloader::BeatmapInfo bi{downloader::BeatmapType::Sid, -1, true};
-                for (auto &i : j) {
-                    bi.id = i["id"];
-                    LOGD("Post Mapper download: bid=%d", bi.id);
-                    Downloader::GetInstance().postSearch(bi);
+                        bi.id = i["id"];
+                        LOGD("Post Mapper download: bid=%d", bi.id);
+                        Downloader::GetInstance().postSearch(bi);
+                    }
                 }
+            } catch (...) {
+                LOGW("Cannot parse Mapper download response!");
+                LOGD("Response: %s", response.c_str());
+                thiz.canceled = true;
+                GuiHelper::ShowWarnToast(lang.getTextCStr("MapperDownloadFailedParseFailed"));
             }
-        } catch (...) {
-            LOGW("Cannot parse Mapper download response!");
-            GuiHelper::ShowWarnToast(lang.getTextCStr("MapperDownloadFailedParseFailed"));
+            break;
         }
-        break;
+        case 404: {
+            LOGW("Mapper Download failed: user(%d) not found!", thiz.id);
+            thiz.canceled = true;
+            GuiHelper::ShowWarnToast(lang.getTextCStr("MapperDownloadFailedUserNotFound"), thiz.id);
+            break;
+        }
+        default: {
+            LOGW("Mapper Download failed with http code: %d", code);
+            thiz.canceled = true;
+            GuiHelper::ShowWarnToast(lang.getTextCStr("MapperDownloadFailedResponseCodeNotOk"), code);
+            break;
+        }
+        }
+    };
+
+    MultiDownload::GetInstance().doRequest(req);
+}
+
+void DoMostPlayedDownload(int id, int beg, int end) {
+    // https://osu.ppy.sh/users/{}/beatmapsets/most_played?
+
+    auto &lang = i18n::I18nManager::GetInstance();
+
+    if (beg > end) {
+        LOGW("Invalid bp download range: beg=%d, end=%d", beg + 1, end);
+        GuiHelper::ShowWarnToast(lang.getTextCStr("InvalidDLRange"), beg + 1, end);
+        return;
     }
-    case 404: {
-        LOGW("Mapper Download failed: user(%d) not found!", id);
-        GuiHelper::ShowWarnToast(lang.getTextCStr("MapperDownloadFailedUserNotFound"), id);
-        break;
-    }
-    default: {
-        LOGW("Mapper Download failed with http code: %d", code);
-        GuiHelper::ShowWarnToast(lang.getTextCStr("MapperDownloadFailedResponseCodeNotOk"), code);
-        break;
-    }
-    }
+
+    GuiHelper::ShowInfoToast(lang.getTextCStr("StartDownloadMostPlayed"), id, beg + 1, end);
+
+    MultiDownload::Task req;
+    req.url = std::format("https://osu.ppy.sh/users/{}/beatmapsets/most_played?", id);
+    req.beg = beg;
+    req.end = end;
+    req.id = id;
+    req.callback = [&](const int code, std::string &response, MultiDownload::Task &thiz) {
+        switch (code) {
+        case 200: {
+            try {
+                if (nlohmann::json j = nlohmann::json::parse(response); j.is_array() && !j.empty()) {
+                    if (j.size() < 100)
+                        thiz.canceled = true;
+                    downloader::BeatmapInfo bi{downloader::BeatmapType::Bid, -1, true};
+                    for (auto &i : j) {
+                        if (!i.contains("beatmap_id"))
+                            continue;
+
+                        bi.id = i["beatmap_id"];
+                        LOGD("Post MostPlayed download: bid=%d", bi.id);
+                        Downloader::GetInstance().postSearch(bi);
+                    }
+                }
+            } catch (...) {
+                LOGW("Cannot parse most played download response!");
+                LOGD("Response: %s", response.c_str());
+                thiz.canceled = true;
+                GuiHelper::ShowWarnToast(lang.getTextCStr("MostPlayedDownloadFailedParseFailed"));
+            }
+            break;
+        }
+        case 404: {
+            LOGW("Most played Download failed: user(%d) not found!", thiz.id);
+            thiz.canceled = true;
+            GuiHelper::ShowWarnToast(lang.getTextCStr("MostPlayedDownloadFailedUserNotFound"), thiz.id);
+            break;
+        }
+        default: {
+            LOGW("Most played Download failed with http code: %d", code);
+            thiz.canceled = true;
+            GuiHelper::ShowWarnToast(lang.getTextCStr("MostPlayedDownloadFailedResponseCodeNotOk"), code);
+            break;
+        }
+        }
+    };
+
+    MultiDownload::GetInstance().doRequest(req);
 }
 
 void MultiDownload::drawMain() {
@@ -275,7 +374,7 @@ void MultiDownload::drawMain() {
         static int id = 0;
         ImGui::InputInt("UID", &id);
 
-        static int begend[] = {1, MAX_FAV_DL_COUNT};
+        static int begend[] = {1, 100};
         ImGui::DragInt2(lang.getTextCStr("Range"), begend, 1, 1, MAX_FAV_DL_COUNT, "%d");
         clamp(begend, 1, begend[1]);
         clamp(begend + 1, begend[0], MAX_FAV_DL_COUNT);
@@ -300,7 +399,7 @@ void MultiDownload::drawMain() {
         static int id = 0;
         ImGui::InputInt("UID", &id);
 
-        static int begend[] = {1, MAX_MAPPER_DL_COUNT};
+        static int begend[] = {1, 100};
         ImGui::DragInt2(lang.getTextCStr("Range"), begend, 1, 1, MAX_MAPPER_DL_COUNT, "%d");
         clamp(begend, 1, begend[1]);
         clamp(begend + 1, begend[0], MAX_MAPPER_DL_COUNT);
@@ -314,4 +413,65 @@ void MultiDownload::drawMain() {
 #undef MAX_MAPPER_DL_COUNT
 #pragma endregion
 
+#pragma region Most Play DL
+#define MAX_MOST_PLAY_DL_COUNT 99999
+    {
+        ImGui::BeginGroupPanel(lang.getTextCStr("MostPlayedDownloader"));
+
+        static int id = 0;
+        ImGui::InputInt("UID", &id);
+
+        static int begend[] = {1, 100};
+        ImGui::DragInt2(lang.getTextCStr("Range"), begend, 1, 1, MAX_MOST_PLAY_DL_COUNT, "%d");
+        clamp(begend, 1, begend[1]);
+        clamp(begend + 1, begend[0], MAX_MOST_PLAY_DL_COUNT);
+        ImGui::EndGroupPanel();
+
+        if (ImGui::Button(lang.getTextCStr("Download"))) {
+            DoMostPlayedDownload(id, begend[0] - 1, begend[1]);
+        }
+    }
+#undef MAX_MOST_PLAY_DL_COUNT
+#pragma endregion
+}
+
+[[noreturn]] void MultiDownload::SearchThread() {
+    // auto &lang = i18n::I18nManager::GetInstance();
+    auto &inst = GetInstance();
+    while (true) {
+        auto tsk = inst.m_Queue.pop_front();
+
+        int limit = tsk.end - tsk.beg;
+
+        int code = -1;
+        std::string response;
+
+        if (limit <= 100) {
+            auto url = tsk.url;
+            url.append("limit=").append(std::to_string(limit)).append("&offset=").append(std::to_string(tsk.beg));
+            net::curl_get(url.c_str(), response, &code);
+
+            tsk.callback(code, response, tsk);
+        } else {
+            const int count = (tsk.end - tsk.beg) / 100 + 1;
+            int offset = tsk.beg;
+            for (int i = 0; i < count; ++i) {
+                response.clear();
+                code = -1;
+                auto url = tsk.url;
+                const int curLimit = limit < 100 ? limit : 100;
+                url.append("limit=").append(std::to_string(curLimit)).append("&offset=").append(std::to_string(offset));
+                net::curl_get(url.c_str(), response, &code);
+                tsk.callback(code, response, tsk);
+                if (tsk.canceled) {
+                    LOGD("MultiDL search canceled");
+                    break;
+                }
+
+                offset += 100;
+                limit -= 100;
+            }
+        }
+        LOGI("MultiDL search finished");
+    }
 }
--- Downloader/src/features/MultiDownload.h
+++ Downloader/src/features/MultiDownload.h
@@ -1,4 +1,9 @@
 ﻿#pragma once
+#include <deque>
+#include <functional>
+#include <thread>
+
+#include "BlockingContainer.hpp"
 #include "ui/Feature.h"
 
 
@@ -6,6 +11,23 @@ namespace features {
 
 class MultiDownload : public ui::main::Feature {
     MultiDownload();
+    std::thread t_SearchThread;
+
+public:
+    struct Task {
+        std::string url;
+        int beg, end;
+        // code | response | this
+        std::function<void(int, std::string &, Task &)> callback;
+        bool canceled = false;
+        // for logger
+        int id;
+    };
+
+private:
+    container::Blocking<std::deque<Task>> m_Queue;
+
+    [[noreturn]] static void SearchThread();
 
 public:
     static MultiDownload &GetInstance() {
@@ -13,8 +35,11 @@ public:
         return instance;
     }
 
+    void doRequest(const Task &task);
+
     ui::main::FeatureInfo &getInfo() override;
     void drawMain() override;
+
 };
 
 }
